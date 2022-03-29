b110577c7b5529cecde103165536b2cd3bdca352
Zach King <zacheking10@gmail.com>
2022-03-29 00:02:55 -0600

    Maximum Subarray

---
+++ Easy/MaximumSubarray.ts
@@ -0,0 +1,15 @@
+export const Solution = {
+  maxSubArray: (nums: number[]): number => {
+    let maxSub = nums[0];
+    let curSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+      if (curSum < 0) {
+        curSum = 0;
+      }
+      curSum += nums[i];
+      maxSub = Math.max(maxSub, curSum);
+    }
+    return maxSub;
+  },
+};
