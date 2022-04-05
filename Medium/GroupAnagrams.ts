c0369671907f4b84537892aa1facd3874d653ed2
Zach King <zacheking10@gmail.com>
2022-04-05 08:20:45 -0600

    49. group anagrams

---
+++ Medium/GroupAnagrams.ts
@@ -0,0 +1,33 @@
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// Example 1:
+
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+// Example 2:
+
+// Input: strs = [""]
+// Output: [[""]]
+// Example 3:
+
+// Input: strs = ["a"]
+// Output: [["a"]]
+
+export const Solution = {
+  firstTry: function groupAnagrams(strs: string[]): string[][] {
+    const map: any = {};
+    for (let i = 0; i < strs.length; i++) {
+      const origin = strs[i];
+      const sorted = strs[i].split("").sort().join("");
+      if (map.hasOwnProperty(sorted)) {
+        const prev = map[sorted];
+        prev.push(origin);
+      } else {
+        map[sorted] = [origin];
+      }
+    }
+    return Object.values(map);
+  }, // 120ms, 53.9MB
+};
