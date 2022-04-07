727d28d6f73d61ad0e17d2240d917620120b7050
Zach King <zacheking10@gmail.com>
2022-04-07 02:41:39 -0600

    167. TwoSumII second try

--- Medium/TwoSumII.ts
+++ Medium/TwoSumII.ts
@@ -9,4 +9,18 @@ export const Solution = {
       map[numbers[i]] = i + 1;
     }
   }, // 68ms, 44.3MB - This doesnt actually meet the constraints of the problem though, pointers must be used
+  secondTry: function twoSum(numbers: number[], target: number): number[] {
+    let l = 0;
+    let r = numbers.length - 1;
+    while (l < r) {
+      const curSum = numbers[l] + numbers[r];
+      if (curSum > target) {
+        r--;
+      } else if (curSum < target) {
+        l++;
+      } else {
+        return [l + 1, r + 1];
+      }
+    }
+  }, // 84ms, 43.9MB
 };
